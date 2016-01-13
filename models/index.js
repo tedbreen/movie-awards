184583e650736d0c6be64c774c5e5573971d1033
Edward Breen <ted.breen@gmail.com>
2016-01-12 17:16:07 -0800

    new model: Person
    fixes #3
    
    Includes a name column.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 var Movie = require('./movie.js')
   , Award = require('./award.js')
+  , Person = require('./person.js')
 
 module.exports = function (sequelize) {
   return {
     Movie: Movie(sequelize)
   , Award: Award(sequelize)
+  , Person: Person(sequelize)
   }
 }
