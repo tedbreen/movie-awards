64c6677f5e343ecddeb621f43f89928c4e132bd0
Edward Breen <ted.breen@gmail.com>
2016-01-18 15:01:21 -0800

    data: seed data for golden globe nominations
    fixes #15

--- seeders/20160115160504-nominations.js
+++ seeders/20160115160504-nominations.js
@@ -8,6 +8,7 @@ var commonData = {
 module.exports = {
   up: function (queryInterface) {
     return queryInterface.bulkInsert('nominations', [
+      // oscars
       // actor
       defaults({award_id: 1, category_id: 1, movie_id: 54, person_id: 13}, commonData)
     , defaults({award_id: 1, category_id: 1, movie_id: 49, person_id: 58}, commonData)
@@ -140,6 +141,94 @@ module.exports = {
     , defaults({award_id: 1, category_id: 28, movie_id: 49}, commonData)
     , defaults({award_id: 1, category_id: 28, movie_id: 51}, commonData)
     , defaults({award_id: 1, category_id: 28, movie_id: 36}, commonData)
+    // golden globes
+    // picture - drama
+    , defaults({award_id: 2, category_id: 20, movie_id: 51, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 20, movie_id: 24}, commonData)
+    , defaults({award_id: 2, category_id: 20, movie_id: 9}, commonData)
+    , defaults({award_id: 2, category_id: 20, movie_id: 31}, commonData)
+    , defaults({award_id: 2, category_id: 20, movie_id: 27}, commonData)
+    // picture - musica/comedy
+    , defaults({award_id: 2, category_id: 21, movie_id: 49, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 21, movie_id: 22}, commonData)
+    , defaults({award_id: 2, category_id: 21, movie_id: 32}, commonData)
+    , defaults({award_id: 2, category_id: 21, movie_id: 53}, commonData)
+    , defaults({award_id: 2, category_id: 21, movie_id: 38}, commonData)
+    // actress - drama
+    , defaults({award_id: 2, category_id: 6, movie_id: 27, person_id: 12, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 6, movie_id: 8, person_id: 78}, commonData)
+    , defaults({award_id: 2, category_id: 6, movie_id: 9, person_id: 15}, commonData)
+    , defaults({award_id: 2, category_id: 6, movie_id: 9, person_id: 75}, commonData)
+    , defaults({award_id: 2, category_id: 6, movie_id: 42, person_id: 8}, commonData)
+    // actor - drama
+    , defaults({award_id: 2, category_id: 2, movie_id: 51, person_id: 45, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 2, movie_id: 12, person_id: 87}, commonData)
+    , defaults({award_id: 2, category_id: 2, movie_id: 33, person_id: 62}, commonData)
+    , defaults({award_id: 2, category_id: 2, movie_id: 42, person_id: 24}, commonData)
+    , defaults({award_id: 2, category_id: 2, movie_id: 54, person_id: 13}, commonData)
+    // actress - comedy
+    , defaults({award_id: 2, category_id: 7, movie_id: 22, person_id: 36, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 7, movie_id: 19, person_id: 47}, commonData)
+    , defaults({award_id: 2, category_id: 7, movie_id: 32, person_id: 61}, commonData)
+    , defaults({award_id: 2, category_id: 7, movie_id: 47, person_id: 49}, commonData)
+    , defaults({award_id: 2, category_id: 7, movie_id: 53, person_id: 10}, commonData)
+    // actor - comedy/musical
+    , defaults({award_id: 2, category_id: 3, movie_id: 49, person_id: 58, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 3, movie_id: 14, person_id: 3}, commonData)
+    , defaults({award_id: 2, category_id: 3, movie_id: 38, person_id: 18}, commonData)
+    , defaults({award_id: 2, category_id: 3, movie_id: 20, person_id: 51}, commonData)
+    , defaults({award_id: 2, category_id: 3, movie_id: 38, person_id: 80}, commonData)
+    // actress - supporting
+    , defaults({award_id: 2, category_id: 8, movie_id: 33, person_id: 44, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 8, movie_id: 45, person_id: 35}, commonData)
+    , defaults({award_id: 2, category_id: 8, movie_id: 54, person_id: 31}, commonData)
+    , defaults({award_id: 2, category_id: 8, movie_id: 58, person_id: 34}, commonData)
+    , defaults({award_id: 2, category_id: 8, movie_id: 16, person_id: 8}, commonData)
+    // actor - supporting
+    , defaults({award_id: 2, category_id: 4, movie_id: 13, person_id: 81, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 4, movie_id: 2, person_id: 63}, commonData)
+    , defaults({award_id: 2, category_id: 4, movie_id: 6, person_id: 32}, commonData)
+    , defaults({award_id: 2, category_id: 4, movie_id: 7, person_id: 52}, commonData)
+    , defaults({award_id: 2, category_id: 4, movie_id: 23, person_id: 67}, commonData)
+    // director
+    , defaults({award_id: 2, category_id: 12, movie_id: 51, person_id: 5, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 12, movie_id: 9, person_id: 83}, commonData)
+    , defaults({award_id: 2, category_id: 12, movie_id: 24, person_id: 29}, commonData)
+    , defaults({award_id: 2, category_id: 12, movie_id: 31, person_id: 86}, commonData)
+    , defaults({award_id: 2, category_id: 12, movie_id: 49, person_id: 72}, commonData)
+    // screenplay
+    , defaults({award_id: 2, category_id: 23, movie_id: 33, person_id: 1, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 23, movie_id: 27, person_id: 25}, commonData)
+    , defaults({award_id: 2, category_id: 23, movie_id: 31, person_id: 43}, commonData)
+    , defaults({award_id: 2, category_id: 23, movie_id: 31, person_id: 86}, commonData)
+    , defaults({award_id: 2, category_id: 23, movie_id: 45, person_id: 70}, commonData)
+    , defaults({award_id: 2, category_id: 23, movie_id: 38, person_id: 2}, commonData)
+    , defaults({award_id: 2, category_id: 23, movie_id: 38, person_id: 16}, commonData)
+    // animated
+    , defaults({award_id: 2, category_id: 9, movie_id: 21, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 9, movie_id: 28}, commonData)
+    , defaults({award_id: 2, category_id: 9, movie_id: 44}, commonData)
+    , defaults({award_id: 2, category_id: 9, movie_id: 50}, commonData)
+    , defaults({award_id: 2, category_id: 9, movie_id: 5}, commonData)
+    // foreign language
+    , defaults({award_id: 2, category_id: 15, movie_id: 30, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 15, movie_id: 25}, commonData)
+    , defaults({award_id: 2, category_id: 15, movie_id: 40}, commonData)
+    , defaults({award_id: 2, category_id: 15, movie_id: 41}, commonData)
+    , defaults({award_id: 2, category_id: 15, movie_id: 43}, commonData)
+    // original score
+    , defaults({award_id: 2, category_id: 17, movie_id: 45, person_id: 27, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 17, movie_id: 9, person_id: 14}, commonData)
+    , defaults({award_id: 2, category_id: 17, movie_id: 33, person_id: 19}, commonData)
+    , defaults({award_id: 2, category_id: 17, movie_id: 42, person_id: 7}, commonData)
+    , defaults({award_id: 2, category_id: 17, movie_id: 51, person_id: 9}, commonData)
+    , defaults({award_id: 2, category_id: 17, movie_id: 51, person_id: 76}, commonData)
+    // original song
+    , defaults({award_id: 2, category_id: 18, movie_id: 35, winner: true}, commonData)
+    , defaults({award_id: 2, category_id: 18, movie_id: 17}, commonData)
+    , defaults({award_id: 2, category_id: 18, movie_id: 18}, commonData)
+    , defaults({award_id: 2, category_id: 18, movie_id: 23}, commonData)
+    , defaults({award_id: 2, category_id: 18, movie_id: 58}, commonData)
     ])
   },
 
