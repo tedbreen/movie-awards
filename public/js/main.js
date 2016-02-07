1c4e895ee78de03bb002b0799f6e14c6a5f833ac
Edward Breen <ted.breen@gmail.com>
2016-02-07 09:42:28 -0800

    internal: setup browserify/watchify

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var markup = (
+  <div className="movieAwards">
+    <h1>{`Movie Awards`}</h1>
+  </div>
+)
+
+ReactDOM.render(markup, document.getElementById('react'))
